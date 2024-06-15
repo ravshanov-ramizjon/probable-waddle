26966a940977754d7befe7cee5352e67490a3c4c
ravshanov-ramizjon <ravsanovramiz@gmail.com>
2024-06-14 15:15:41 -0930

    probable-waddle

---
+++ index.js
@@ -0,0 +1,7 @@
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+let even = numbers.filter(num => num % 2 === 0);
+let odd = numbers.filter(num => num % 2 === 1);
+
+console.log(even + " Even:");
+console.log(odd +" Odd:");
+
